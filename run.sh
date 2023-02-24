dc6990a07b731557fc766bbf3b98f1426e581b9f
Judicaël Grasset <judicael.grasset@meteo.fr>
2023-02-24 15:40:25 +0000

    nope

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 set -x
 ./clean.sh
-exec /home/gmap/mrpm/marguina/install/gmkpack_support/wrapper/PGI231/mpif90 -c -mp -fPIC -Mlarge_arrays -g -acc=gpu -I.D[5] -I.D[10] -I.D[15] -I.D[74] -I.D[78] bator_decodnetcdf_mod.F90
+exec /home/gmap/mrpm/marguina/install/gmkpack_support/wrapper/PGI231/mpif90 -c -mp -g -acc=gpu -I.D[5] -I.D[10] -I.D[15] -I.D[74] -I.D[78] bator_decodnetcdf_mod.F90
